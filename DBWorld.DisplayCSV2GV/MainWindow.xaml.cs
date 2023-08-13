fa7c2f7c250895cb54f946c801587d649f89cae3
Alyaa Awny <alyaaawny6@gmail.com>
2023-08-13 23:09:18 +0200

    Add project files.

---
+++ DBWorld.DisplayCSV2GV/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+
+namespace DBWorld.DisplayCSV2GV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = @"Receipts.csv";
+            Receipts receiptsInfo = new Receipts();
+
+            string[] receiptsArr;
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Business Unit", typeof(string));
+            dataTable.Columns.Add("Receipt Method ID", typeof(string));
+            dataTable.Columns.Add("Remittance Bank", typeof(string));
+            dataTable.Columns.Add("Remittance Bank Account", typeof(string));
+            dataTable.Columns.Add("Receipt Number", typeof(string));
+            dataTable.Columns.Add("Receipt Amount", typeof(string));
+            dataTable.Columns.Add("Receipt Date", typeof(string));
+            dataTable.Columns.Add("Accounting Date", typeof(string));
+            dataTable.Columns.Add("Conversion Date", typeof(string));
+            dataTable.Columns.Add("Currency", typeof(string));
+            dataTable.Columns.Add("Conversion Rate Type", typeof(string));
+            dataTable.Columns.Add("Conversion Rate", typeof(string));
+            dataTable.Columns.Add("Customer Name", typeof(string));
+            dataTable.Columns.Add("Customer Account Number", typeof(string));
+            dataTable.Columns.Add("Customer Site Number", typeof(string));
+            dataTable.Columns.Add("Invoice Number Reference", typeof(string));
+            dataTable.Columns.Add("Invoice Amount", typeof(string));
+            dataTable.Columns.Add("Comments", typeof(string));
+
+            using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    receiptsArr = sr.ReadLine().Split(",");
+                    //receiptsInfo.BusinessUnit = receiptsArr[0];
+                    //receiptsInfo.ReceiptMethodID = receiptsArr[1];
+                    //receiptsInfo.RemittanceBank = receiptsArr[2];
+                    //receiptsInfo.RemittanceBankAccount = receiptsArr[3];
+                    //receiptsInfo.ReceiptNumber = receiptsArr[4];
+                    //receiptsInfo.ReceiptAmount = receiptsArr[5];
+                    //receiptsInfo.ReceiptDate = receiptsArr[6];
+                    //receiptsInfo.AccountingDate = receiptsArr[7];
+                    //receiptsInfo.ConversionDate = receiptsArr[8];
+                    //receiptsInfo.Currency = receiptsArr[9];
+                    //receiptsInfo.ConversionRateType = receiptsArr[10];
+                    //receiptsInfo.ConversionRate = receiptsArr[9];
+                    //receiptsInfo.CustomerName = receiptsArr[12];
+                    //receiptsInfo.CustomerAccountNumber = receiptsArr[13];
+                    //receiptsInfo.CustomerSiteNumber = receiptsArr[14];
+                    //receiptsInfo.InvoiceNumberReference = receiptsArr[10];
+                    //receiptsInfo.InvoiceAmount = receiptsArr[11];
+                    //receiptsInfo.Comments = receiptsArr[17];
+                    dataTable.Rows.Add(receiptsArr);
+                }
+                DataView dataView = new DataView(dataTable);
+                dtGridView.ItemsSource = dataView;
+
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            string csvFilePath = Server.MapPath("~/input.csv");
+            string xlsxFilePath = Server.MapPath("~/output.xlsx");
+
+            ConvertCsvToXlsx(csvFilePath, xlsxFilePath);
+
+            FileInfo xlsxFile = new FileInfo(xlsxFilePath);
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment; filename=output.xlsx");
+            Response.BinaryWrite(xlsxFile.GetAsByteArray());
+            Response.End();
+
+            return null;
+
+
+        }
+    }
+}
---
+++ DBWorld.DisplayCSV2GV/Receipts.cs
@@ -0,0 +1,62 @@
+﻿using OfficeOpenXml;
+using System.IO;
+
+namespace DBWorld.DisplayCSV2GV
+{
+    public class Receipts
+    {
+        public string BusinessUnit { get; set; } = string.Empty;
+        public string ReceiptMethodID { get; set; } = string.Empty;
+        public string RemittanceBank { get; set; } = string.Empty;
+        public string RemittanceBankAccount { get; set; } = string.Empty;
+        public string ReceiptNumber { get; set; } = string.Empty;
+        public string ReceiptAmount { get; set; } = string.Empty;
+        public string ReceiptDate { get; set; } = string.Empty;
+        public string AccountingDate { get; set; } = string.Empty;
+        public string ConversionDate { get; set; } = string.Empty;
+        public string Currency { get; set; } = string.Empty;
+        public string ConversionRateType { get; set; } = string.Empty;
+        public string ConversionRate { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerAccountNumber { get; set; } = string.Empty;
+        public string CustomerSiteNumber { get; set; } = string.Empty;
+        public string InvoiceNumberReference { get; set; } = string.Empty;
+        public string InvoiceAmount { get; set; } = string.Empty;
+        public string Comments { get; set; } = string.Empty;
+
+        public Receipts()
+        {
+
+        }
+        static void ConvertCsvToXlsx(string csvFilePath, string xlsxFilePath)
+        {
+            FileInfo csvFile = new FileInfo(csvFilePath);
+            FileInfo xlsxFile = new FileInfo(xlsxFilePath);
+
+            using (ExcelPackage package = new ExcelPackage(xlsxFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
+                int row = 1;
+
+                using (StreamReader reader = new StreamReader(csvFile.FullName))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string[] csvLine = reader.ReadLine().Split(',');
+                        for (int col = 1; col <= csvLine.Length; col++)
+                        {
+                            worksheet.Cells[row, col].Value = csvLine[col - 1];
+                        }
+                        row++;
+                    }
+                }
+
+                package.Save();
+            }
+        }
+
+
+    }
+
+}
+
